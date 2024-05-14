5757af4f7e8db8aef1a54f1070ca87f19a7dd294
Rafael M V <rafaelmendoncavaz@hotmail.com>
2024-05-14 11:40:48 -0300

    Tarefa 4 concluída

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,29 @@
 const modalHeaderButton = document.querySelector('.header__button--modal')
 const modalFaqButton = document.querySelector('.button__faq--modal')
 const modal = document.querySelector('.modal__container')
+const closeModal = document.querySelector('.modal--close')
+const modalInput = document.querySelector('.modal__input')
 
 modalHeaderButton.addEventListener('click', function(e) {
     e.stopPropagation()
 
     modal.showModal()
+    closeModal.addEventListener('click', function(e) {
+        e.stopPropagation()
+
+        modal.close()
+        modalInput.value = ''
+    })
 })
 
 modalFaqButton.addEventListener('click', function(e) {
     e.stopPropagation()
 
     modal.showModal()
+    closeModal.addEventListener('click', function(e) {
+        e.stopPropagation()
+
+        modal.close()
+        modalInput.value = ''
+    })
 })
